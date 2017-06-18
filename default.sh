369fd11e4a3d7653389a56d0b7a50eb04f8cec10
yiwensong.com <yiwensong.com@gmail.com>
2017-06-18 02:29:05 +0000

    git configs

--- default.sh
+++ default.sh
@@ -5,6 +5,9 @@ sudo apt-get install git
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 
 git clone https://github.com/yiwensong/gcp-setup.git ~/gcp-setup
+git config --global user.email 'yiwensong.com@gmail.com'
+git config --global user.name 'yiwensong.com'
+git config --global push.default simple
 cd ~/gcp-setup
 sh unzip.sh
 cd ~
