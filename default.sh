8a6dc1171964e8cd6b777a841355add45fe6f161
Yiwen Song <songzgy@gmail.com>
2017-06-18 14:02:10 +1200

    Update default.sh

--- default.sh
+++ default.sh
@@ -3,3 +3,9 @@ sudo apt-get upgrade
 sudo apt-get install git
 
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+
+git clone https://github.com/yiwensong/gcp-setup.git ~/gcp-setup
+cd ~/gcp-setup
+sh unzip.sh
+cd ~
+rm -rf ~/gcp-setup
